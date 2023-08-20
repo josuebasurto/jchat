3a34f6100c9e34124f3e67fbcfe35ed233f0b63b
Josue Basurto <josuebasurto@gmail.com>
2023-08-19 23:28:53 -0700

    Ahora todo lo administra Channel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,38 +1,30 @@
 <template>
-    <div>
-      <h2>Home View</h2>
-      <form @submit="redirectToRandomChannel">
-        <label for="name">Nombre:</label>
-        <input v-model="name" id="name" />
-        <button type="submit">OK</button>
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'HomeView',
-    data() {
-      return {
-        name: '',
-      };
+
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  mounted() {
+    this.redirectToRandomChannel();
+  },
+  methods: {
+    redirectToRandomChannel() {
+      const randomCode = this.generateRandomCode();
+      this.$router.push(`/channel/${randomCode}`);
     },
-    methods: {
-      redirectToRandomChannel(event) {
-        event.preventDefault();
-        const randomCode = this.generateRandomCode();
-        this.$router.push(`/channel/${randomCode}`);
-      },
-      generateRandomCode() {
-        const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-        let code = '';
-        for (let i = 0; i < 6; i++) {
-          const randomIndex = Math.floor(Math.random() * characters.length);
-          code += characters[randomIndex];
-        }
-        return code;
-      },
+    generateRandomCode() {
+      const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+      let code = '';
+      for (let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        code += characters[randomIndex];
+      }
+      return code;
     },
-  };
+  },
+};
   </script>
   
+  <style>
+</style>
